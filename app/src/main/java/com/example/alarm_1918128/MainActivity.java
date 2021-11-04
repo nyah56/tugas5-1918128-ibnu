41117cb830e8ec3d102c23c06635d6b5a6a2868e
nyah56 <x6perb@gmail.com>
2021-11-04 19:43:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/alarm_1918128/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.alarm_1918128;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements
+        TimePickerDialog.OnTimeSetListener {
+    private TextView mTextView;
+    private ImageView mImageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.TextView);
+        mImageView=findViewById(R.id.ImageView);
+        //memunculkan dialog timepicker menggunakan library dari android
+        Button buttonTimePicker = (Button)
+                findViewById(R.id.button_time_picker);
+        buttonTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new
+                        TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(),"TimePicker");
+                                                        }
+                                                    });
+        //untuk menggagalkan alarm yang sudah disetel
+        Button buttonCancelAlarm =
+                findViewById(R.id.button_cancel);
+        buttonCancelAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelAlarm();
+            }
+        });
+    }
+    //menangkap inputan jam kalian lalu memulai alarm
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int
+            minute) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY,hourOfDay);
+        c.set(Calendar.MINUTE,minute);
+        c.set(Calendar.SECOND,0);
+        updateTimeText(c);
+        startAlarm(c);
+    }
+    //mengganti text view
+    private void updateTimeText(Calendar c){
+        String timeText = "Alarm set for: ";
+        int vue;
+        timeText +=
+                DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
+        mTextView.setText(timeText);
+        mImageView.setImageResource(R.drawable.ic_baseline_alarm_on_24);
+    }
+    //memulai alarm
+    private void startAlarm(Calendar c){
+        AlarmManager alarmManager = (AlarmManager)
+                getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this,AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this,1,intent,0);
+        if(c.before(Calendar.getInstance())){
+            c.add(Calendar.DATE,1);
+        }
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP,c.getTimeInMillis()
+                ,pendingIntent);
+    }
+    //menggagalkan alarm
+    private void cancelAlarm(){
+        AlarmManager alarmManager = (AlarmManager)
+                getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this,AlertReceiver.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getBroadcast(this,1,intent,0);
+        alarmManager.cancel(pendingIntent);
+        mTextView.setText("Alarm Canceled");
+        mImageView.setImageResource(R.drawable.ic_baseline_alarm_off_24);
+    }
+}
